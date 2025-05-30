d884489520eb272fe4b86989798bad6f3c42aa8a
rajkaran <rajkaran1505@gmail.com>
2025-05-30 13:34:59 +0530

    Update

--- src/app/AppRoutes.jsx
+++ src/app/AppRoutes.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home/Home";
+import Portfolio from "../pages/Portfolio/Portfolio";
+
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/portfolio" element={<Portfolio />} />
+      
     </Routes>
   );
 }
---
+++ src/pages/Portfolio/Portfolio.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../Components/Navbar/Navbar'
+
+export default function Portfolio() {
+  return (
+    <>
+    <div className="hero bg-black w-100% h-screen flex flex-col items-center justify-center p-3">
+        <h1 className='text-5xl text-white font-sans'>Cooked with <span className='text-light-cream font-semibold'>Code</span> </h1>
+        <h2 className='text-5xl text-white mt-lg font-sans'>Served With <span className='text-light-cream font-semibold'>Care</span></h2>
+
+    </div>
+    </>
+  )
+}
