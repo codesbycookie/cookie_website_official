7fd293589808083d11f7addfa119bedfe6ddf764
vickydecodes <darklegendvs@gmail.com>
2025-05-30 14:24:45 +0530

    update

--- src/pages/Components/CookieBtn/CookieBtn.jsx
+++ src/pages/Components/CookieBtn/CookieBtn.jsx
@@ -4,7 +4,7 @@ import './CookieBtn.css'
 
 export default function CookieBtn({name}) {
   return (
-    <button className='btn border-t-2 bg-black border-3 rounded-sm border-muted cookie-btn'>
+    <button className='btn border-t-2 bg-black border-3 rounded-xl border-muted cookie-btn'>
        <img src="/imgs/cookie_white.png" className='h-5 w-5' alt="" /> {name}
     </button>
   )
--- src/pages/Home/Components/Hero/Hero.jsx
+++ src/pages/Home/Components/Hero/Hero.jsx
@@ -2,6 +2,20 @@ import React from 'react'
 
 export default function Hero() {
   return (
-    <div>Hero</div>
+    <div className='h-screen bg-black text-white'>
+      <div className="mx-50">
+        <div className="pt-30 flex flex-col justify-center items-center">
+          <h1 className='font-mona-sans font-semibold text-center text-9xl'>Medium length hero <br /> heading goes here</h1>
+        <p className='text-center mt-20 text-2xl w-250'>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat.
+        </p>
+        <div className="pt-30 text-center">
+         <button className='btn p-8 mx-3 bg-white text-black font-bold rounded-xl'>Explore more</button>
+         <button className='btn p-8 mx-3 border-t-1 border-3 border-muted rounded-xl bg-black text-white'>Contact Us</button>
+
+        </div>
+        </div>
+      </div>
+    </div>
   )
 }
