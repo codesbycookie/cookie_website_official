b8eeb44c14fa22ef049f22e7f0cc14ac13820a2e
vickydecodes <darklegendvs@gmail.com>
2025-07-25 00:48:42 +0530

    Update AppRoutes.jsx

--- src/app/AppRoutes.jsx
+++ src/app/AppRoutes.jsx
@@ -55,7 +55,7 @@ export default function AppRoutes() {
           }
         />
         <Route
-          path="/services"
+          path="/service"
           element={
             <MainLayout>
               <Service />
